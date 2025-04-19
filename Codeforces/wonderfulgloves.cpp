62ed0c833dd6d3272708c5cf0782804c943ed85d
cduckling <abhirajmallangi@outlook.com>
2025-04-19 12:49:52 -0400

    Add files via upload

---
+++ Codeforces/wonderfulgloves.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+#define tc ll t; cin >> t; while (t--) solve();
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(a) (a).begin(), (a).end()
+#define pb(i) push_back(i)
+#define eb(i) emplace_back(i)
+#define inf (ll) 1e18
+#define f first
+#define s second
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vector<ll>> vvi;
+typedef vector<vector<vector<ll>>> vvvi;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<vector<double>>> vvvd;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef pair<ll, ll> pii;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vector<pair<ll, ll>>> vvpii;
+typedef vector<vector<vector<pair<ll, ll>>>> vvvpii;
+typedef tuple<ll, ll, ll> tiii;
+typedef vector<tuple<ll, ll, ll>> vtiii;
+typedef vector<vector<tuple<ll, ll, ll>>> vvtiii;
+typedef vector<vector<vector<tuple<ll, ll, ll>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
+typedef set<ll> si;
+typedef vector<set<ll>> vsi;
+typedef vector<vector<set<ll>>> vvsi;
+typedef set<pair<ll, ll>> spii;
+typedef vector<set<pair<ll, ll>>> vspii;
+typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
+typedef map<ll, double> mid;
+typedef vector<map<ll, ll>> vmii;
+typedef vector<map<ll, double>> vmid;
+typedef vector<map<ll, bool>> vmib;
+
+void solve() {
+    ll n, k; cin >> n >> k;
+
+    vi l (n + 1); for (ll i = 1; i <= n; i++) cin >> l[i];
+    vi r (n + 1); for (ll i = 1; i <= n; i++) cin >> r[i];
+
+    ll ops = 0; for (ll i = 1; i <= n; i++) ops += max(l[i], r[i]);
+
+    vi other (n + 1); for (ll i = 1; i <= n; i++) other[i] = min(l[i], r[i]); sort(all(other), greater<ll>());
+
+    for (ll i = 0; i < k - 1; i++) ops += other[i];
+
+    cout << ops + 1 << "\n";
+}
+
+int main() {
+    io
+    tc
+
+    return 0;
+}
