59ecc2bbb2b62bed6a2e456831d38200eccf6f54
cduckling <abhirajmallangi@outlook.com>
2025-06-26 13:09:50 -0400

    Add files via upload

---
+++ Codeforces/commongenerator.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
+#define tc int t; cin >> t; while (t--) solve();
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(a) (a).begin(), (a).end()
+#define pb(i) push_back(i)
+#define eb(i) emplace_back(i)
+#define inf (ll) 1e18
+#define fir first
+#define sec second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<vector<double>>> vvvd;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<pair<int, int>>> vvpii;
+typedef vector<vector<vector<pair<int, int>>>> vvvpii;
+typedef tuple<int, int, int> tiii;
+typedef vector<tuple<int, int, int>> vtiii;
+typedef vector<vector<tuple<int, int, int>>> vvtiii;
+typedef vector<vector<vector<tuple<int, int, int>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
+typedef set<int> si;
+typedef vector<set<int>> vsi;
+typedef vector<vector<set<int>>> vvsi;
+typedef set<pair<int, int>> spii;
+typedef vector<set<pair<int, int>>> vspii;
+typedef map<int, int> mii;
+typedef map<int, bool> mib;
+typedef map<int, double> mid;
+typedef vector<map<int, int>> vmii;
+typedef vector<map<int, double>> vmid;
+typedef vector<map<int, bool>> vmib;
+
+#define fail {cout << -1 << "\n"; return;}
+
+int p[400001];
+
+void solve() {
+    int n; cin >> n;
+
+    vi a (n + 1); for (int i = 1; i <= n; i++) cin >> a[i];
+
+    int k = 2; for (int i = 1; i <= n; i++) if (p[a[i]] == a[i]) {
+        if (k > 2) fail k = a[i];
+    }
+
+    if (k == 2) {
+        cout << k << "\n";
+    } else {
+        for (int i = 1; i <= n; i++) {
+            if (a[i] == k) continue;
+
+            if (a[i] % 2 == 0) {
+                if (a[i] < 2 * k) fail
+            } else {
+                if (a[i] - p[a[i]] < 2 * k) fail
+            }
+        }
+
+        cout << k << "\n";
+    }
+}
+
+int main() {
+    io
+
+    for (int i = 2; i <= 400000; i++) {
+        if (p[i]) continue;
+
+        for (int j = i; j <= 400000; j += i) if (!p[j]) p[j] = i;
+    }
+
+    tc
+
+    return 0;
+}
