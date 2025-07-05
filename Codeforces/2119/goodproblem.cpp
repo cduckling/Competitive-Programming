7567d0b0148796c3d9b5e937b6a2b3f451a7f5a8
cduckling <abhirajmallangi@outlook.com>
2025-07-05 12:43:44 -0400

    Add files via upload

---
+++ Codeforces/2119/goodproblem.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
+#define tc ll t; cin >> t; while (t--) solve();
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(a) (a).begin(), (a).end()
+#define pb(i) push_back(i)
+#define eb(i) emplace_back(i)
+#define inf (ll) 1e18
+#define fir first
+#define sec second
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vector<ll>> vvi;
+typedef vector<vector<vector<ll>>> vvvi;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<vector<double>>> vvvd;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef pair<ll, ll> pii;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vector<pair<ll, ll>>> vvpii;
+typedef vector<vector<vector<pair<ll, ll>>>> vvvpii;
+typedef tuple<ll, ll, ll> tiii;
+typedef vector<tuple<ll, ll, ll>> vtiii;
+typedef vector<vector<tuple<ll, ll, ll>>> vvtiii;
+typedef vector<vector<vector<tuple<ll, ll, ll>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
+typedef set<ll> si;
+typedef vector<set<ll>> vsi;
+typedef vector<vector<set<ll>>> vvsi;
+typedef set<pair<ll, ll>> spii;
+typedef vector<set<pair<ll, ll>>> vspii;
+typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
+typedef map<ll, double> mid;
+typedef vector<map<ll, ll>> vmii;
+typedef vector<map<ll, double>> vmid;
+typedef vector<map<ll, bool>> vmib;
+
+void solve() {
+    ll n, l, r, k; cin >> n >> l >> r >> k;
+
+    ll bit = 0; for (ll i = 0; i < 60; i++) if (1LL<<i&l) bit = i; bit = 1LL<<(bit + 1);
+
+    if (n == 2) {
+        cout << -1 << "\n";
+    } else if (n % 2 != 0) {
+        cout << l << "\n";
+    } else if (bit > r) {
+        cout << -1 << "\n";
+    } else if (k > n - 2) {
+        cout << bit << "\n";
+    } else {
+        cout << l << "\n";
+    }
+}
+
+int main() {
+    io
+    tc
+
+    return 0;
+}
