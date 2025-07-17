f415404d269f61adaa04167f72fd7395b1da4f80
cduckling <abhirajmallangi@outlook.com>
2025-07-17 10:29:34 -0400

    Add files via upload

--- Codeforces/inamountain.cpp
+++ Codeforces/inamountain.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
 
-#define tc int t; cin >> t; while (t--) solve();
+#define tc ll t; cin >> t; while (t--) solve();
 #define io ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(a) (a).begin(), (a).end()
 #define pb(i) push_back(i)
@@ -22,47 +22,57 @@ using namespace std;
 #define sec second
 
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vi;
+typedef vector<vector<ll>> vvi;
+typedef vector<vector<vector<ll>>> vvvi;
 typedef vector<double> vd;
 typedef vector<vector<double>> vvd;
 typedef vector<vector<vector<double>>> vvvd;
 typedef vector<bool> vb;
 typedef vector<vector<bool>> vvb;
 typedef vector<vector<vector<bool>>> vvvb;
-typedef pair<int, int> pii;
-typedef vector<pair<int, int>> vpii;
-typedef vector<vector<pair<int, int>>> vvpii;
-typedef vector<vector<vector<pair<int, int>>>> vvvpii;
-typedef tuple<int, int, int> tiii;
-typedef vector<tuple<int, int, int>> vtiii;
-typedef vector<vector<tuple<int, int, int>>> vvtiii;
-typedef vector<vector<vector<tuple<int, int, int>>>> vvvtiii;
+typedef pair<ll, ll> pii;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<vector<pair<ll, ll>>> vvpii;
+typedef vector<vector<vector<pair<ll, ll>>>> vvvpii;
+typedef tuple<ll, ll, ll> tiii;
+typedef vector<tuple<ll, ll, ll>> vtiii;
+typedef vector<vector<tuple<ll, ll, ll>>> vvtiii;
+typedef vector<vector<vector<tuple<ll, ll, ll>>>> vvvtiii;
 typedef vector<string> vs;
 typedef vector<vector<string>> vvs;
-typedef set<int> si;
-typedef vector<set<int>> vsi;
-typedef vector<vector<set<int>>> vvsi;
-typedef set<pair<int, int>> spii;
-typedef vector<set<pair<int, int>>> vspii;
-typedef map<int, int> mii;
-typedef map<int, bool> mib;
-typedef map<int, double> mid;
-typedef vector<map<int, int>> vmii;
-typedef vector<map<int, double>> vmid;
-typedef vector<map<int, bool>> vmib;
+typedef set<ll> si;
+typedef vector<set<ll>> vsi;
+typedef vector<vector<set<ll>>> vvsi;
+typedef set<pair<ll, ll>> spii;
+typedef vector<set<pair<ll, ll>>> vspii;
+typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
+typedef map<ll, double> mid;
+typedef vector<map<ll, ll>> vmii;
+typedef vector<map<ll, double>> vmid;
+typedef vector<map<ll, bool>> vmib;
 typedef vector<char> vc;
 typedef vector<vector<char>> vvc;
 
 void solve() {
-    int n, k; cin >> n >> k;
+    ll n, k; cin >> n >> k;
 
-    vi a (n + 1); for (int i = 1; i <= n; i++) cin >> a[i], a[i];
+    vi a (n + 1); for (ll i = 1; i <= n; i++) cin >> a[i], a[i] %= k;
 
-    int b = 0; for (int i = 1; i <= n; i++) b += a[i];
+    ll res = 0;
 
+    priority_queue<ll> pq;
 
+    for (ll i = 1; i <= n; i++) {
+        if (a[i] > a[i - 1]) {
+            pq.push(-(a[i] - a[i - 1])); res += -pq.top(); pq.pop();
+        } else {
+            pq.push(-(k + a[i] - a[i - 1]));
+        }
+    }
+
+    cout << res << "\n";
 }
 
 int main() {
