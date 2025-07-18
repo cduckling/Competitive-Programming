e5dc60aece3750ed1c85ff40f1f622ef899fa16a
cduckling <abhirajmallangi@outlook.com>
2025-07-18 10:51:52 -0400

    Update lca.cpp

--- Guide to Competitive Programming/Tree Algorithms/lca.cpp
+++ Guide to Competitive Programming/Tree Algorithms/lca.cpp
@@ -2,20 +2,24 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_map>
 #include <queue>
 #include <stack>
 #include <algorithm>
 #include <numeric>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
 #define tc int t; cin >> t; while (t--) solve();
 #define io ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(a) (a).begin(), (a).end()
 #define pb(i) push_back(i)
 #define eb(i) emplace_back(i)
-#define ceil(a, b) ((a + b - 1) / b)
-#define inf (ll) 1e18
-#define fail cout << -1 << "\n"; return;
+#define inf 1e9
+#define fir first
+#define sec second
 
 typedef long long ll;
 typedef vector<int> vi;
@@ -31,36 +35,72 @@ typedef pair<int, int> pii;
 typedef vector<pair<int, int>> vpii;
 typedef vector<vector<pair<int, int>>> vvpii;
 typedef vector<vector<vector<pair<int, int>>>> vvvpii;
-typedef tuple<int, int, int> tii;
-typedef vector<tuple<int, int, int>> vtii;
-typedef vector<vector<tuple<int, int, int>>> vvtii;
-typedef vector<vector<vector<tuple<int, int, int>>>> vvvtii;
+typedef tuple<int, int, int> tiii;
+typedef vector<tuple<int, int, int>> vtiii;
+typedef vector<vector<tuple<int, int, int>>> vvtiii;
+typedef vector<vector<vector<tuple<int, int, int>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
 typedef set<int> si;
 typedef vector<set<int>> vsi;
 typedef vector<vector<set<int>>> vvsi;
+typedef set<pair<int, int>> spii;
+typedef vector<set<pair<int, int>>> vspii;
 typedef map<int, int> mii;
 typedef map<int, bool> mib;
 typedef map<int, double> mid;
 typedef vector<map<int, int>> vmii;
 typedef vector<map<int, double>> vmid;
 typedef vector<map<int, bool>> vmib;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
 
-void dfs(const int ver, const int par, vi& dep, vvi& up, const vvi& adj) {
-    up[ver][0] = par; for (int i = 1; i <= 30; i++) up[ver][i] = up[up[ver][i - 1]][i - 1];
+struct SEGTREE {
+    int n; vi t, dep;
 
-    for (const int i : adj[ver]) if (i != par) dep[i] = dep[ver] + 1, dfs(i, ver, dep, up, adj);
+    SEGTREE(const int n) {
+        this->n = n; t.assign(2 * n, 0);
+    }
+
+    SEGTREE(const vi& a, const vi& dep) : SEGTREE(a.size()) {
+        this->dep = dep; for (int i = 0; i < n; i++) set(i, a[i]);
+    }
+
+    int query(int a, int b) {
+        a += n, b += n;
+
+        int val = 0;
+
+        while (a <= b) {
+            if (a % 2 == 1) {
+                if (dep[t[a]] < dep[val]) val = t[a]; a++;
+            }
+
+            if (b % 2 == 0) {
+                if (dep[t[b]] < dep[val]) val = t[b]; b--;
+            }
+
+            a /= 2, b /= 2;
+        }
+
+        return val;
+    }
+
+    void set(int pos, const int val) {
+        pos += n; t[pos] = val;
+
+        for (pos /= 2; pos >= 1; pos /= 2) {
+            t[pos] = t[2 * pos]; if (dep[t[2 * pos]] > dep[t[2 * pos + 1]]) t[pos] = t[2 * pos + 1];
+        }
+    }
+};
+
+void dfs(const int ver, const int par, vi& dep, const vvi& adj) {
+    for (const int i : adj[ver]) if (i != par) dep[i] = dep[ver] + 1, dfs(i, ver, dep, adj);
 }
 
-int anc(int ver, const int k, const vvi& up) {
-    for (int j = 0; j <= 30; j++) if ((1<<j&k) > 0) ver = up[ver][j]; return ver;
-}
-
-int lca(int a, int b, const vi& dep, const vvi& up) {
-    if (dep[a] < dep[b]) swap(a, b); a = anc(a, dep[a] - dep[b], up);
-
-    if (a == b) return a;
-
-    for (int i = 30; i >= 0; i--) if (up[a][i] != up[b][i]) a = up[a][i], b = up[b][i]; return up[a][0];
+void dfs(const int ver, const int par, int& pos, vi& tour, const vvi& adj) {
+    tour[pos] = ver; for (const int i : adj[ver]) if (i != par) dfs(i, ver, ++pos, tour, adj), tour[++pos] = ver;
 }
 
 int main() {
@@ -73,16 +113,16 @@ int main() {
         adj[u].pb(v), adj[v].pb(u);
     }
 
-    vi dep (n + 1); dep[1] = 1;
+    vi dep (n + 1); dfs(1, -1, dep, adj); dep[0] = inf;
 
-    vvi up (n + 1, vi (31));
+    int pos = 1; vi tour (2 * n + 1); dfs(1, -1, pos, tour, adj); tour.pop_back();
 
-    dfs(1, 0, dep, up, adj);
+    vi r (n + 1); for (int i = 1; i < 2 * n; i++) if (!r[tour[i]]) r[tour[i]] = i;
 
-    for (int i = 0; i < q; i++) {
-        int a, b; cin >> a >> b;
+    SEGTREE lca (tour, dep);
 
-        cout << lca(a, b, dep, up) << "\n";
+    for (int i = 1; i <= q; i++) {
+        int u, v; cin >> u >> v; if (r[u] > r[v]) swap(u, v); cout << lca.query(r[u], r[v]) << "\n";
     }
 
     return 0;
