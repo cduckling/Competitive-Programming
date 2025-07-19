e348a7e833d86a738ab6ad59fa1c92a1c9f94536
cduckling <abhirajmallangi@outlook.com>
2025-07-19 09:39:11 -0400

    Update dsu.cpp

--- Guide to Competitive Programming/Graph Algorithms/dsu.cpp
+++ Guide to Competitive Programming/Graph Algorithms/dsu.cpp
@@ -1,30 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-using namespace std;
-
-#define io ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define pb(i) push_back(i);
-#define inf 1e18;
-
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<pair<int, int>> vpii;
-typedef vector<bool> vb;
-typedef set<int> si;
-typedef map<int, int> mi;
-typedef map<int, vector<int>> mvi;
-typedef map<int, pair<int, int>> mpii;
-typedef map<int, bool> mb;
-
 struct DSU {
     vi pars, sizes;
 
@@ -32,27 +5,21 @@ struct DSU {
       for (int i = 0; i < n; i++) pars[i] = i;
     }
 
-    int root(const int ver) {
-        return pars[ver] == ver ? ver : (pars[ver] = root(pars[ver]));
+    int root(const int v) {
+        return pars[v] == v ? v : (pars[v] = root(pars[v]));
     }
 
-    void unite(const int a, const int b) {
-        if (root(a) == root(b)) return;
+    void unite(const int u, const int v) {
+        if (root(u) == root(v)) return;
 
-        int A = root(a), B = root(b);
+        int U = root(u), V = root(v);
 
-        if (sizes[A] < sizes[B]) swap(A, B);
+        if (sizes[U] < sizes[V]) swap(U, V);
 
-        sizes[A] += sizes[B]; pars[B] = A;
+        sizes[U] += sizes[V]; pars[V] = U;
     }
 
     bool connected(const int u, const int v) {
         return root(u) == root(v);
     }
 };
-
-int main() {
-    io
-
-    return 0;
-}
