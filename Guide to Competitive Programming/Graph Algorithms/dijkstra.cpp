39c4dea6baf20cfc92b1747d134561ef6b384537
cduckling <abhirajmallangi@outlook.com>
2025-08-03 11:08:54 -0400

    Update dijkstra.cpp

--- Guide to Competitive Programming/Graph Algorithms/dijkstra.cpp
+++ Guide to Competitive Programming/Graph Algorithms/dijkstra.cpp
@@ -2,56 +2,81 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_map>
 #include <queue>
 #include <stack>
 #include <algorithm>
-#include <cmath>
 #include <numeric>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
+#define tc int t; cin >> t; while (t--) solve();
 #define io ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define pb(i) push_back(i);
-#define inf 1e9
+#define all(a) (a).begin(), (a).end()
+#define pb(a) push_back(a)
+#define eb(a, b) emplace_back(a, b)
+#define inf (ll) 1e18
+#define fir first
+#define sec second
 
 typedef long long ll;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
-typedef vector<pair<int, int>> vpii;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<vector<double>>> vvvd;
 typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<pair<int, int>>> vvpii;
+typedef vector<vector<vector<pair<int, int>>>> vvvpii;
+typedef tuple<int, int, int> tiii;
+typedef vector<tuple<int, int, int>> vtiii;
+typedef vector<vector<tuple<int, int, int>>> vvtiii;
+typedef vector<vector<vector<tuple<int, int, int>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
 typedef set<int> si;
-typedef map<int, int> mi;
-typedef map<int, vector<int>> mvi;
-typedef map<int, pair<int, int>> mpii;
-typedef map<int, bool> mb;
+typedef vector<set<int>> vsi;
+typedef vector<vector<set<int>>> vvsi;
+typedef set<pair<int, int>> spii;
+typedef vector<set<pair<int, int>>> vspii;
+typedef map<int, int> mii;
+typedef map<int, bool> mib;
+typedef map<int, double> mid;
+typedef vector<map<int, int>> vmii;
+typedef vector<map<int, double>> vmid;
+typedef vector<map<int, bool>> vmib;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
 
 int main() {
     io
 
     int n, m; cin >> n >> m;
 
-    vector<vector<pii>> adj (n); for (int i {0}; i < m; ++i) {
+    vvpii adj (n + 1); for (int i = 1; i <= m; i++) {
         int u, v, w; cin >> u >> v >> w;
-
-        adj[u].emplace_back(v, w); adj[v].emplace_back(u, w);
+        adj[u].eb(v, w), adj[v].eb(u, w);
     }
 
-    vi dist (n, inf);
+    vi dist (n + 1, inf); dist[1] = 0;
 
-    priority_queue<pii> pq; pq.emplace(0, 0); dist[0] = 0;
+    priority_queue<pii> pq; pq.emplace(0, 1);
 
     while (!pq.empty()) {
-        const int v = pq.top().second; pq.pop();
+        const int ver = pq.top().sec; pq.pop();
 
-        for (const pii i : adj[v]) {
-            if (dist[v] + i.second < dist[i.first]) {
-                dist[i.first] = dist[v] + i.second;
-                pq.emplace(-dist[i.first], i.first);
-            }
+        for (const pii i : adj[ver]) if (dist[ver] + i.sec < dist[i.fir]) {
+            dist[i.fir] = dist[ver] + i.sec;
+            pq.emplace(-dist[i.fir], i.fir);
         }
     }
-
-    for (const int i : dist) cout << i << " "; cout << "\n";
 
     return 0;
 }
