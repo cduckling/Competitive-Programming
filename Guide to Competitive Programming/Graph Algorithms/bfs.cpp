4edc5872b6bb051d8b34abdf5f877984926182bb
cduckling <abhirajmallangi@outlook.com>
2025-08-15 08:19:52 -0400

    Update bfs.cpp

--- Guide to Competitive Programming/Graph Algorithms/bfs.cpp
+++ Guide to Competitive Programming/Graph Algorithms/bfs.cpp
@@ -2,52 +2,80 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_map>
 #include <queue>
 #include <stack>
 #include <algorithm>
-#include <cmath>
 #include <numeric>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
+#define tc int t; cin >> t; while (t--) solve();
 #define io ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define pb(i) push_back(i);
-#define inf 1e18;
+#define all(a) (a).begin(), (a).end()
+#define pb(a) push_back(a)
+#define eb(a, b) emplace_back(a, b)
+#define inf (ll) 1e18
+#define fir first
+#define sec second
 
 typedef long long ll;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
-typedef vector<pair<int, int>> vpii;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<vector<double>>> vvvd;
 typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<pair<int, int>>> vvpii;
+typedef vector<vector<vector<pair<int, int>>>> vvvpii;
+typedef tuple<int, int, int> tiii;
+typedef vector<tuple<int, int, int>> vtiii;
+typedef vector<vector<tuple<int, int, int>>> vvtiii;
+typedef vector<vector<vector<tuple<int, int, int>>>> vvvtiii;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
 typedef set<int> si;
-typedef map<int, int> mi;
-typedef map<int, vector<int>> mvi;
-typedef map<int, pair<int, int>> mpii;
-typedef map<int, bool> mb;
+typedef vector<set<int>> vsi;
+typedef vector<vector<set<int>>> vvsi;
+typedef set<pair<int, int>> spii;
+typedef vector<set<pair<int, int>>> vspii;
+typedef map<int, int> mii;
+typedef map<int, bool> mib;
+typedef map<int, double> mid;
+typedef vector<map<int, int>> vmii;
+typedef vector<map<int, double>> vmid;
+typedef vector<map<int, bool>> vmib;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
 
 int main() {
     io
 
     int n, m; cin >> n >> m;
 
-    vvi adj (n); for (int i {0}; i < m; ++i) {
+    vvi adj (n + 1); for (int i = 1; i <= m; i++) {
         int u, v; cin >> u >> v;
-        adj[u].pb(v); adj[v].pb(u);
+        adj[u].pb(v), adj[v].pb(u);
     }
 
-    vb vis (n); vi dist (n);
+    vb vis (n + 1); vi dist (n + 1); vis[1] = true;
 
-    queue<int> q; q.push(0); vis[0] = true;
+    queue<int> q; q.push(1);
 
     while (!q.empty()) {
-        const int u = q.front(); q.pop();
+        const int ver = q.front(); q.pop();
 
-        for (auto v : adj[u]) {
-            if (!vis[v]) {
-                q.push(v); vis[v] = true, dist[v] = dist[u] + 1;
-            }
-        }
+        for (const int i : adj[ver]) if (!vis[i]) vis[i] = true, dist[i] = dist[ver] + 1, q.push(i);
     }
+
+    for (int i = 1; i <= n; i++) cout << dist[i] << " ";
 
     return 0;
 }
